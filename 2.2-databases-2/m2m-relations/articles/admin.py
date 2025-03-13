b879df3bf48caff543dbced26e821ad2e6ac47ef
Neto test <akochetkov1312@gmail.com>
2025-03-13 15:26:08 +0300

    Add models

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 
 from .models import Article
 
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if 'is_main' in form.cleaned_data:
+                if form.cleaned_data['is_main']:
+                    count += 1
+            if count == 0:
+                raise ValidationError('Какая-то ошибка')
+            if count > 1:
+                raise ValidationError('Какая-то ошибка')
+            else:
+                pass
+        return super().clean()
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
+
+
+@admin.register(Article)
+class ObjectAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
